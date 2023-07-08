6b6049ba8b402536775191f91154afce75ef11af
lsaudon <lsaudon@gmail.com>
2023-07-08 10:06:14 +0200

    refactor: report command

--- test/src/commands/report_command_test.dart
+++ test/src/commands/report_command_test.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:file/file.dart';
 import 'package:file/memory.dart';
 import 'package:license_harvest_cli/src/command_runner.dart';
 import 'package:mason_logger/mason_logger.dart';
@@ -12,27 +13,37 @@ import 'input.dart';
 class _MockLogger extends Mock implements Logger {}
 
 void main() {
-  group('report', () {
-    test('with a license file', () async {
-      final MemoryFileSystem fileSystem = MemoryFileSystem.test(
+  late FileSystem fileSystem;
+  late _MockLogger logger;
+  late LicenseHarvestCliCommandRunner commandRunner;
+  setUp(
+    () {
+      fileSystem = MemoryFileSystem.test(
         style: Platform.isWindows
             ? FileSystemStyle.windows
             : FileSystemStyle.posix,
       );
-
-      fileSystem.file(Uri.parse(p.join(feAnalyzerSharedRootUri, 'LICENSE')))
-        ..createSync(recursive: true)
-        ..writeAsStringSync(feAnalyzerSharedLicense);
-
-      fileSystem.file(p.join('.dart_tool', 'package_config.json'))
-        ..createSync(recursive: true)
-        ..writeAsStringSync(packageConfigJsonContent);
-
-      final _MockLogger logger = _MockLogger();
-      final int exitCode = await LicenseHarvestCliCommandRunner(
+      logger = _MockLogger();
+      commandRunner = LicenseHarvestCliCommandRunner(
         logger: logger,
         fileSystem: fileSystem,
-      ).run(<String>['report']);
+      );
+    },
+  );
+  group('report', () {
+    test('with a license file', () async {
+      <String, String>{
+        p.join(feAnalyzerSharedRootUri, 'LICENSE'): feAnalyzerSharedLicense,
+        p.join(nodePreambleRootUri, 'LICENSE'): nodePreambleLicense,
+        p.join('.dart_tool', 'package_config.json'): packageConfigJsonContent,
+      }.forEach(
+        (final String path, final String content) =>
+            fileSystem.file(Uri.parse(path))
+              ..createSync(recursive: true)
+              ..writeAsStringSync(content),
+      );
+
+      final int exitCode = await commandRunner.run(<String>['report']);
 
       expect(exitCode, ExitCode.success.code);
 
@@ -42,15 +53,14 @@ void main() {
           '_fe_analyzer_shared;BSD-3-Clause;https://pub.dev/packages/_fe_analyzer_shared/license',
         ),
       );
+      verify(
+        () => logger.info(
+          'node_preamble;BSD-3-Clause,MIT;https://pub.dev/packages/node_preamble/license',
+        ),
+      );
     });
 
     test('without a license file', () async {
-      final MemoryFileSystem fileSystem = MemoryFileSystem.test(
-        style: Platform.isWindows
-            ? FileSystemStyle.windows
-            : FileSystemStyle.posix,
-      );
-
       fileSystem
           .file(Uri.parse(p.join(feAnalyzerSharedRootUri)))
           .createSync(recursive: true);
@@ -59,11 +69,7 @@ void main() {
         ..createSync(recursive: true)
         ..writeAsStringSync(packageConfigJsonContent);
 
-      final _MockLogger logger = _MockLogger();
-      final int exitCode = await LicenseHarvestCliCommandRunner(
-        logger: logger,
-        fileSystem: fileSystem,
-      ).run(<String>['report']);
+      final int exitCode = await commandRunner.run(<String>['report']);
 
       expect(exitCode, ExitCode.success.code);
 
